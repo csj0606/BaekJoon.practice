5ff37e5248a0b5b96e215343ba0da4737dfe1097
LeeSangDo <71632350+LEE-SangDo@users.noreply.github.com>
2023-03-24 13:56:12 +0900

    Update 9506.py
    다른 방법으로 풀어보기

--- 9506.py
+++ 9506.py
@@ -3,6 +3,16 @@ while True:
     if n ==-1:
         break
 
+/// 약수를 구하는 경우, 에라토스테네스의 체를 이용한 방법으로 풀 수 있다. 
+
+    sum=0
+    slist=[]
+    for i in range(2, n/2):
+        if n % i ==0:
+            sum+=i
+            slist.append(str(i))
+
+/// 본래대로 푼 방법           
     sum=0
     slist =[]
     for i in range(1,n):
