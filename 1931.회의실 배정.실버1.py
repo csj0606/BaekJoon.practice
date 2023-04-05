48551c35202c151169ac8782783f938987656879
csj0606 <csjoon0606@gmail.com>
2023-04-05 16:05:58 +0900

    4.05

--- 1931.회의실 배정.실버1.py
+++ 1931.회의실 배정.실버1.py
@@ -7,7 +7,6 @@ for i in range(N):
     s, e = map(int, sys.stdin.readline().split())
     time[i][0] = s
     time[i][1] = e
-#1.빨리 끝나는가, 2.시작이 빠른가 두 가지를 고려해 정렬
 time.sort(key = lambda x: (x[1], x[0]))
 
 cnt = 1
