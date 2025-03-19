f3748685173b92a6e24f62cd297ce5b46e414d3d
최성준 <48979141+csj0606@users.noreply.github.com>
2025-03-19 15:30:16 +0900

    25.03.19
    fixed

--- (5430)AC.py
+++ (5430)AC.py
@@ -1,38 +1,36 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
-
 n = int(input())
-
-for _ in range(n):
-    p = input().rstrip()
-    n = int(input())
-    arr = input().rstrip()[1:-1].split(",")
-    queue = deque()
-
-    rev, front, back = 0, 0, len(queue)-1
-    flag = 0
-    if n == 0:
-        queue = []
-        front = 0
-        back = 0
-
-    for j in p:
-        if j == 'R':
-            rev += 1
-        elif j == 'D':
-            if len(queue) < 1:
-                flag = 1
-                print("error")
+for i in range(n):
+    a = input().strip()
+    m = int(input())
+    flag = 1
+    arr = input().strip()
+    dq = deque(arr[1:-1].split(','))
+    if m == 0:
+        dq = deque()
+    R = 0
+    for i in range(len(a)):
+        if a[i] == 'R':
+            R += 1
+        elif a[i] == 'D':
+            if len(dq) == 0:
+                print('error')
+                flag = 0
                 break
             else:
-                if rev % 2 == 0:
-                    queue.popleft()
+                if R % 2 == 0:
+                    dq.popleft()
                 else:
-                    queue.pop()
+                    dq.pop()
+
     if flag == 0:
-        if rev % 2 == 0:
-            print("[" + ",".join(queue) + "]")
+        continue
+    else:
+        if R % 2 == 0:
+            print('[' + ",".join(dq) + ']')
         else:
-            queue.reverse()
-            print("[" + ",".join(queue) + "]")
+            dq.reverse()
+            print('[' + ",".join(dq) + ']')
