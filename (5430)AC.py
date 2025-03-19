bd4b5fb63632b8994f8cadadfff1057cacd6b4f6
최성준 <48979141+csj0606@users.noreply.github.com>
2025-03-19 15:27:33 +0900

    25.03.19
    Daily commit

---
+++ (5430)AC.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    p = input().rstrip()
+    n = int(input())
+    arr = input().rstrip()[1:-1].split(",")
+    queue = deque()
+
+    rev, front, back = 0, 0, len(queue)-1
+    flag = 0
+    if n == 0:
+        queue = []
+        front = 0
+        back = 0
+
+    for j in p:
+        if j == 'R':
+            rev += 1
+        elif j == 'D':
+            if len(queue) < 1:
+                flag = 1
+                print("error")
+                break
+            else:
+                if rev % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
+    if flag == 0:
+        if rev % 2 == 0:
+            print("[" + ",".join(queue) + "]")
+        else:
+            queue.reverse()
+            print("[" + ",".join(queue) + "]")
