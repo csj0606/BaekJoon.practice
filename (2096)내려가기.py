5c63ff8ce5194471077e04f92359617e6f17ad11
최성준 <48979141+csj0606@users.noreply.github.com>
2025-04-11 21:54:38 +0900

    Update (2096)내려가기.py

--- (2096)내려가기.py
+++ (2096)내려가기.py
@@ -11,5 +11,4 @@ for _ in range(N - 1):
     maxDP = [arr[0] + max(maxDP[0], maxDP[1]), arr[1] + max(maxDP), arr[2] + max(maxDP[1], maxDP[2])]
     minDP = [arr[0] + min(minDP[0], minDP[1]), arr[1] + min(minDP), arr[2] + min(minDP[1], minDP[2])]
 
-
 print(max(maxDP), min(minDP))
