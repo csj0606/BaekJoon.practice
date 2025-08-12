9fea9ed87f66c52280fbbcfdc59e4e9812c7acfa
최성준 <48979141+csj0606@users.noreply.github.com>
2025-08-12 15:03:18 +0900

    Update (1987)알파벳.py

--- (1987)알파벳.py
+++ (1987)알파벳.py
@@ -1,25 +1,29 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 r, c = map(int, input().split())
-board = [list(map(lambda x:ord(x)-65, input())) for _ in range(r)]
-alpha = [False] * 26
-max_ = 1
-d = [(1,0), (-1,0), (0,1), (0,-1)]
+board = [list(input().strip()) for _ in range(r)]
 
-def DFS(x, y, cnt):
-    global max_
-    max_ = max(max_, cnt)
-    
-    for i in range(4):
-        nx, ny = x + d[i][0], y + d[i][1]
-        
+dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+visited_states = set()
+max_len = 1
+
+def DFS(x, y, mask, cnt):
+    global max_len
+    max_len = max(max_len, cnt)
+
+    if (x, y, mask) in visited_states:
+        return
+    visited_states.add((x, y, mask))
+
+    for dx, dy in dirs:
+        nx, ny = x + dx, y + dy
         if 0 <= nx < r and 0 <= ny < c:
-            if not alpha[board[nx][ny]]:
-                alpha[board[nx][ny]] = True
-                DFS(nx,ny,cnt+1)
-                alpha[board[nx][ny]] = False
-                    
-alpha[board[0][0]] = True
-DFS(0,0,max_)
-print(max_)
+            idx = ord(board[nx][ny]) - ord('A')
+            if not (mask & (1 << idx)):
+                DFS(nx, ny, mask | (1 << idx), cnt + 1)
+
+start_mask = 1 << (ord(board[0][0]) - ord('A'))
+DFS(0, 0, start_mask, 1)
+print(max_len)
